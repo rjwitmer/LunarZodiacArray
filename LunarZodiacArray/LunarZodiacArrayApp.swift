09a39997ecf10e52feee9c5575a61eeb8bfb8698
Bob Witmer <bwitmer@golden.net>
2023-07-04 11:24:36 -0400

    Initial Commit

---
+++ LunarZodiacArray/LunarZodiacArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacArrayApp.swift
+//  LunarZodiacArray
+//
+//  Created by Bob Witmer on 2023-07-04.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
