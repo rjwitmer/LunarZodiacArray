4f734375d0c78e8d95b1e72f191a772358aa6d92
Bob Witmer <bwitmer@golden.net>
2023-07-05 16:46:14 -0400

    Completed

--- LunarZodiacArray/ContentView.swift
+++ LunarZodiacArray/ContentView.swift
@@ -8,12 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animalNames = ["RAT",
+                                      "OX",
+                                      "TIGER",
+                                      "RABBIT",
+                                      "DRAGON",
+                                      "SNAKE",
+                                      "HORSE",
+                                      "GOAT",
+                                      "MONKEY",
+                                      "ROOSTER",
+                                      "DOG",
+                                      "PIG"
+    ]
+    @State private var year = 2023
+    @State private var index = 3
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text("\(String(year))")
+                .font(.largeTitle)
+                .fontWeight(.black)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image("\(animalNames[index])")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.accentColor)
+            
+            Text("\(animalNames[index])")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    year -= 1
+                    
+                    if index > 0 {
+                        index -= 1
+                    } else {
+                        index = 11
+                    }
+                } label: {
+                    Image(systemName: "chevron.left")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                
+                Spacer()
+                
+                Button {
+                    year += 1
+                    
+                    if index < 11 {
+                        index += 1
+                    } else {
+                        index = 0
+                    }
+                } label: {
+                    Image(systemName: "chevron.right")
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.accentColor)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            }
         }
         .padding()
     }
